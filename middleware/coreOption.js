b6c15834254e3fede5e327bd2000bfeb21da3cc1
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-11-09 20:20:04 +0530

    Update coreOption.js

--- middleware/coreOption.js
+++ middleware/coreOption.js
@@ -1,4 +1,4 @@
-const whiteList = ['http://localhost:3000' , 'https://todouserlist.vercel.app']
+const whiteList = ['http://localhost:3000' , 'https://todouserlist.vercel.app', 'todouserlist.vercel.app']
 
 export const corsOptions = {
     origin:(origin , callBack)=>{
